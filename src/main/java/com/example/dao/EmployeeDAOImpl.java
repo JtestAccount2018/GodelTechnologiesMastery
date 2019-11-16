5cd3fce1201bd79619338b064dc1abe3d5bb5617
Ulad Proc <bgpoints@mail.ru>
2019-11-16 16:00:30 +0300

    add 404 page

--- src/main/java/com/example/dao/EmployeeDAOImpl.java
+++ src/main/java/com/example/dao/EmployeeDAOImpl.java
@@ -16,13 +16,9 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @PropertySource("classpath:/EmployeeDao.properties")
-@EnableTransactionManagement
-@Component
 @Repository
 public class EmployeeDAOImpl implements EmployeeDAO {
 
